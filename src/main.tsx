e5761391069fa5ee3602de9b152fc26ecf72e43e
sven <sven.mueller@extern.unite.eu>
2025-04-23 19:55:56 +0200

    revert: browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,32 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {RouterProvider, Navigate, createHashRouter} from 'react-router-dom'
+import {createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import Kanji from './components/Kanji'
 import Vocabulary from './components/Vocabulary'
 import Settings from './components/Settings'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
     {
         path: import.meta.env.BASE_URL ?? '/',
-        element: <App/>,
+        element: <App />,
         children: [
             {
                 path: '',
-                element: <Navigate to="kanji" replace/>
+                element: <Navigate to="kanji" replace />
             },
             {
                 path: 'kanji',
-                element: <Kanji/>
+                element: <Kanji />
             },
             {
                 path: 'vocabulary',
-                element: <Vocabulary/>
+                element: <Vocabulary />
             },
             {
                 path: 'settings',
-                element: <Settings/>
+                element: <Settings />
             }
         ]
     }
@@ -34,6 +34,6 @@ const router = createHashRouter([
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RouterProvider router={router}/>
+        <RouterProvider router={router} />
     </StrictMode>,
 )
