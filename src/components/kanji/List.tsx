ea8f9e56cc066258b719d0314c490eb27b673647
sven <sven.mueller@extern.unite.eu>
2025-04-25 22:05:13 +0200

    fix: simplefied listings

--- src/components/kanji/List.tsx
+++ src/components/kanji/List.tsx
@@ -3,26 +3,16 @@ import SelectableButton from '../shared/SelectableButton.tsx';
 import { ValidationContext } from '../../contexts/ValidationContext.tsx';
 
 const List: FC = () => {
-  const { validItems, items, selectedIndex, setSelectedIndex } = useContext(ValidationContext);
+  const { items } = useContext(ValidationContext);
 
   return (
     <div
       className="grid gap-2 auto-rows-auto border-t-pink-400 border-t-2 pt-6"
       style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(10rem, 1fr))' }}
     >
-      {items.map(
-        (item, index) =>
-          'kanji' in item && (
-            <SelectableButton
-              key={item.id}
-              selected={selectedIndex === index}
-              valid={validItems.includes(item.id)}
-              onClick={() => setSelectedIndex(index)}
-              term={item.kanji}
-              type={'kanji'}
-            />
-          )
-      )}
+      {items.map((item, index) => (
+        <SelectableButton key={item.id} position={index} type={'kanji'} />
+      ))}
     </div>
   );
 };
--- src/components/shared/SelectableButton.tsx
+++ src/components/shared/SelectableButton.tsx
@@ -1,22 +1,25 @@
-import { type FC, useCallback } from 'react';
+import { type FC, useContext, useMemo } from 'react';
 import { NoAnswer } from './icons/NoAnswer.tsx';
 import { CorrectAnswer } from './icons/CorrectAnswer.tsx';
+import { ValidationContext } from '../../contexts/ValidationContext.tsx';
 
 interface SelectableButtonProps {
-  selected: boolean;
-  valid: boolean;
-  onClick: () => void;
-  term: string;
+  position: number;
   type: 'kanji' | 'vocabulary';
 }
 
-const SelectableButton: FC<SelectableButtonProps> = ({ term, selected, valid, onClick, type }) => {
-  const getButtonClass = useCallback(() => {
-    const background = type === 'kanji' ? 'bg-pink-400' : 'bg-purple-400';
-    return selected ? `${background} text-white` : 'text-gray-400';
+const SelectableButton: FC<SelectableButtonProps> = ({ position, type }) => {
+  const { validItems, items, selectedIndex, setSelectedIndex } = useContext(ValidationContext);
+  const selected= position === selectedIndex;
+  const valid= validItems.includes(items[position].id)
+  const term= "kanji" in items[position] ? items[position].kanji : items[position].word;
+
+  const backgroundColor = useMemo(() => {
+    if (selected) return type === 'kanji' ? 'bg-pink-400' : 'bg-purple-400';
+    return 'text-gray-400';
   }, [selected, type]);
 
-  const getTextColor = useCallback(() => {
+  const textColor = useMemo(() => {
     if (selected) return 'text-white';
     if (valid) return 'text-green-700';
     return 'text-gray-400';
@@ -24,14 +27,12 @@ const SelectableButton: FC<SelectableButtonProps> = ({ term, selected, valid, on
 
   return (
     <button
-      className={`cursor-pointer grid grid-cols-[min-content_auto] items-center gap-2 ${getTextColor()}`}
-      onClick={onClick}
+      className={`cursor-pointer grid grid-cols-[min-content_auto] items-center gap-2 ${backgroundColor} ${textColor}`}
+      onClick={() => setSelectedIndex(position)}
     >
       {!valid && <NoAnswer />}
       {valid && <CorrectAnswer />}
-      <div className={`text-xl text-nowrap text-left rounded-lg px-2 py-1 ${getButtonClass()}`}>
-        {term}
-      </div>
+      <div className={`text-xl text-nowrap text-left rounded-lg px-2 py-1`}>{term}</div>
     </button>
   );
 };
