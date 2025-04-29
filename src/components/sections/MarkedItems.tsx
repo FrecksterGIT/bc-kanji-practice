5ca4257d895291088a6c085e0e72a1e0fda2c2ed
sven <sven.mueller@extern.unite.eu>
2025-04-29 23:41:59 +0200

    feat: implement wanikani provider with caching and updated_after data loading

--- src/components/sections/MarkedItems.tsx
+++ src/components/sections/MarkedItems.tsx
@@ -1,12 +1,20 @@
-import { FC, useContext } from 'react';
+import { FC, useContext, useEffect } from 'react';
 import ValidationProvider from '../../contexts/ValidationProvider.tsx';
 import MainKanji from '../kanji/MainKanji.tsx';
 import { ValidationContext } from '../../contexts/ValidationContext.tsx';
 import MainVocabulary from '../vocabulary/MainVocabulary.tsx';
 import useMarkedItems from '../../hooks/useMarkedItems.ts';
+import { WanikaniProvider } from '../../contexts/WanikaniProvider.tsx';
+import { ResourceType, WanikaniContext } from '../../contexts/WanikaniContext.ts';
 
 const ItemRenderer = () => {
+  const { load } = useContext(WanikaniContext);
   const { item } = useContext(ValidationContext);
+
+  useEffect(() => {
+    load(ResourceType.assignments).then();
+  }, [load]);
+
   if (!item) return null;
   if ('kanji' in item) {
     return <MainKanji />;
@@ -24,9 +32,11 @@ const MarkedItems: FC = () => {
         {error && <p className="text-red-500">Error loading kanji data: {error.message}</p>}
         {!loading && !error && data.length === 0 && <p>No marked items found.</p>}
         {!loading && !error && data && (
-          <ValidationProvider items={data}>
-            <ItemRenderer />
-          </ValidationProvider>
+          <WanikaniProvider>
+            <ValidationProvider items={data}>
+              <ItemRenderer />
+            </ValidationProvider>
+          </WanikaniProvider>
         )}
       </div>
     </div>
---
+++ src/contexts/WanikaniContext.ts
@@ -0,0 +1,42 @@
+import { createContext } from 'react';
+
+export type BasicDataType = {
+  data: unknown;
+  data_updated_at: string;
+  id: number;
+  object: string;
+  url: string;
+};
+
+export enum ResourceType {
+  'subjects' = 'subjects',
+  'assignments' = 'assignments',
+}
+
+export const baseUrl = 'https://api.wanikani.com/';
+
+export const resources: Record<ResourceType, { url: string; params?: Record<string, string> }> = {
+  [ResourceType.subjects]: {
+    url: '/v2/subjects',
+    params: {
+      types: 'kanji,vocabulary,kana_vocabulary',
+    },
+  },
+  [ResourceType.assignments]: {
+    url: '/v2/assignments',
+    params: {
+      started: 'true',
+      burned: 'false',
+    },
+  },
+};
+
+export type WanikaniContextType = {
+  load: <T extends BasicDataType = BasicDataType>(type: ResourceType) => Promise<T[]>;
+};
+
+export const WanikaniContext = createContext<WanikaniContextType>({
+  load: async () => {
+    return [];
+  },
+});
